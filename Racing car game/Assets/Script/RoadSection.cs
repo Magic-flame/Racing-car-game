f57bb1363f9251828298d8e110194ed4368dc450
blue <335224756@qq.com>
2019-06-13 21:15:58 +0800

    数据中心脚本 赛道板块脚本

---
+++ Racing car game/Assets/Script/RoadSection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSection : MonoBehaviour {
+    
+        public Vector3 LastPoint;
+    
+}
--- Racing car game/Assets/Script/RuntimeData.cs
+++ Racing car game/Assets/Script/RuntimeData.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RuntimeData : SingletonMonoBehaviour<RuntimeData> {
     public Transform UIRoot, SceneRoot;
+    public int gameScore;//游戏分数
+    public int gameTime;//游戏时间
+    public Queue<RoadSection> roadQueue;//赛道板块队列
+    public Vector3 LastPoint;//末尾衔接点
+    public int skyBoxNum;//天空盒编号
     public void Init()
     {
         UIRoot = GameObject.Find("UIRoot").transform;
@@ -11,3 +16,4 @@ public class RuntimeData : SingletonMonoBehaviour<RuntimeData> {
         Debug.Log("初始化数据类");
     }
 }
+
